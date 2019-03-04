020c60a457fef0d739f838bd6cfef3c48b1b9fbd
Isaac Smith <isaacns98@gmail.com>
2019-03-04 12:22:58 -0500

    Created template for PiSetup.sh

---
+++ PiSetup/PiSetup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# PiSetup.sh
+# This script goes through the setup of a Raspberry Pi with user prompts.
+# Upon completion, it will create new user(s) with sudo and ssh permissions,
+# remove the default user and password, and setup ssh.
+
+
+# Variables
+
+
+# New Accounts
+
+	# Create Account
+
+	# Give Permissions
+
+# Remove Default Account
+
+# Start SSH
+
+# Configure Network (Stretch Goal) 
+
+# End
