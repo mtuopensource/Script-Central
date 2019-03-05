c6e7159000223228e91b8f4297d99eaa7230ed8e
Isaac Smith <isaacns98@gmail.com>
2019-03-05 18:47:50 -0500

    Added functionality to remove pi usr permissions and start ssh

--- PiSetup/PiSetup.sh
+++ PiSetup/PiSetup.sh
@@ -6,18 +6,39 @@
 
 
 # Variables
-
-
+SSH_CONFIG="/etc/ssh/sshd_config"
+user=""
+another=1
+new="n"
 # New Accounts
-
+while [ $another -eq 1 ]
+do
 	# Create Account
+	read -p "Enter username: " user
+	sudo adduser $user	#possibly change to 'useradd and passwd for simplicity'
 
 	# Give Permissions
+	sudo usermod -aG sudo $user
+	read -p "Create another user? [y(default)/n]" new
+	if [ $new = "n" ]; then
+    	another=0
+	else
+		another=1
+	fi
+done
 
-# Remove Default Account
-
+# Disable 
+echo "Please configure a secure password for the pi user."
+sudo passwd
+echo "Please login to the last created user"
+su $user
+sudo usermod -rG sudo pi
+sudo usermod -s /sbin/nologin pi
+echo "Feel free to completely delete the pi user at a later time."
 # Start SSH
-
+echo "AllowGroups sudo" >> $SSH_CONFIG
+sudo systemctl enable ssh
+sudo systemctl start ssh
 # Configure Network (Stretch Goal) 
 
 # End
